091d26f0a13679ab550b731994e78508228992a5
Peter Kyeyune <peterkayk@gmail.com>
2024-03-23 16:28:49 +0300

    Add Firebase Auth dependency and update SDK version

--- lib/blocs/authentication_bloc/authentication_state.dart
+++ lib/blocs/authentication_bloc/authentication_state.dart
@@ -1,10 +1,3 @@
 part of 'authentication_bloc.dart';
 
-sealed class AuthenticationState extends Equatable {
-  const AuthenticationState();
-  
-  @override
-  List<Object> get props => [];
-}
-
-final class AuthenticationInitial extends AuthenticationState {}
+enum AuthenticationStatus { authenticated, unauthenticated, unknown}
