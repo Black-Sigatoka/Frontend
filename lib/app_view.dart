17dbd6e6fbe033542faa3884c20d94502f1529d9
Peter Kyeyune <peterkayk@gmail.com>
2024-04-02 14:05:25 +0300

    Add file_selector_windows plugin and diagnosis screen

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,5 +1,6 @@
 import 'package:black_sigatoka/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:black_sigatoka/screens/home_page.dart';
+import 'package:black_sigatoka/screens/diagnosis.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
