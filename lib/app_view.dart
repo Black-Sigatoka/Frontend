e62cd8b5e3a7fcbcebe5740803114ea3096d146f
Peter Kyeyune <peterkayk@gmail.com>
2024-04-18 16:29:06 +0300

    Update app_view.dart and home_page.dart with authentication changes

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,10 +1,7 @@
-import 'package:black_sigatoka/blocs/authentication_bloc/authentication_bloc.dart';
-import 'package:black_sigatoka/screens/Diagnosis_page.dart';
-import 'package:black_sigatoka/screens/auth/login_page.dart';
+
 import 'package:black_sigatoka/screens/home_page.dart';
-//import 'package:black_sigatoka/screens/home_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -16,15 +13,7 @@ class MyAppView extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'Poppins',
       ),
-      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
-        builder: (context, state) {
-          if (state.status == AuthenticationStatus.authenticated) {
-            return const HomeScreen();
-          } else {
-            return const LoginScreen();
-          }
-        },
-      ),
+      home: const HomeScreen(),
      );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,8 @@
+import 'package:black_sigatoka/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:black_sigatoka/screens/Diagnosis_page.dart';
 import 'package:black_sigatoka/screens/auth/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,55 +14,66 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/Logo.png',
-              height: 150,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              'Sigatoka Mobile Application'.toUpperCase(),
-              style: const TextStyle(
-                  fontSize: 18,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(height: 25),
-            const Text(
-              'This is a mobile application dedicated to diagnosing and managing Black Leaf Streak efficiently. It harnesses advanced image recognition to identify disease symptoms. The applications provide detailed information about the severity of the disease and recommended actions.',
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
+    return BlocBuilder<AuthenticationBloc, AuthenticationState>(
+      builder: (context, state) {
+        if (state.status == AuthenticationStatus.authenticated) {
+          return const DiagnosisScreen();
+        } else {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Logo.png',
+                    height: 150,
                   ),
-                  backgroundColor: const Color.fromARGB(255, 127, 181, 230)),
-              onPressed: (){
-                Navigator.push(context,  MaterialPageRoute(
-                  builder: (context) => const LoginScreen(),
-                ));
-              },
-              child: Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 95, vertical: 15),
-                child: const Text(
-                  'Get Started',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
+                  const SizedBox(height: 20),
+                  Text(
+                    'Sigatoka Mobile Application'.toUpperCase(),
+                    style: const TextStyle(
+                        fontSize: 18,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600),
                   ),
-                ),
+                  const SizedBox(height: 25),
+                  const Text(
+                    'This is a mobile application dedicated to diagnosing and managing Black Leaf Streak efficiently. It harnesses advanced image recognition to identify disease symptoms. The applications provide detailed information about the severity of the disease and recommended actions.',
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        backgroundColor:
+                            const Color.fromARGB(255, 127, 181, 230)),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LoginScreen(),
+                          ));
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 95, vertical: 15),
+                      child: const Text(
+                        'Get Started',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ),
+            ),
+          );
+        }
+      },
     );
   }
 }
