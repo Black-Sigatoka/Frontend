eed51614057107072281613b171a384233bb9494
Peter Kyeyune <peterkayk@gmail.com>
2024-04-18 16:35:52 +0300

    Update custom_appbar.dart with sign-out functionality

--- lib/custom_widgets/custom_appbar.dart
+++ lib/custom_widgets/custom_appbar.dart
@@ -1,4 +1,6 @@
+import 'package:black_sigatoka/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -11,32 +13,40 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-       backgroundColor: Colors.white,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 30),
-          child: Row(
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/images/Logo.png',
-                  height: 30,
-                  width: 30,
-                ),
+      backgroundColor: Colors.white,
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 30),
+        child: Row(
+          children: [
+            Center(
+              child: Image.asset(
+                'assets/images/Logo.png',
+                height: 30,
+                width: 30,
               ),
-              const SizedBox(width: 5),
-              Center(
-                child: Text(
-                  title.toUpperCase(),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
+            ),
+            const SizedBox(width: 5),
+            Center(
+              child: Text(
+                title.toUpperCase(),
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+      actions: [
+        IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(const SignOutRequired());
+            },
+            icon: const Icon(Icons.logout)
+          )
+      ],
     );
   }
 }
