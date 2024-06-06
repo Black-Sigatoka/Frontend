92519078345c0daca63de18e00912b4350ca6a14
Peter Kyeyune <peterkayk@gmail.com>
2024-06-06 15:30:48 +0300

    "Refactor RecommendationScreen to handle API call and display loading state, error message, and recommendation result"

--- lib/screens/recommendations_page.dart
+++ lib/screens/recommendations_page.dart
@@ -4,7 +4,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 
-
 class RecommendationScreen extends StatefulWidget {
   final String diseaseSeverity;
 
@@ -16,28 +15,43 @@ class RecommendationScreen extends StatefulWidget {
 }
 
 class _RecommendationScreenState extends State<RecommendationScreen> {
-  Future<String?> getRecommendations(String severity) async {
-    //showRecommendations(context, severity);
+  String? recommendation;
+  bool isLoading = false;
+  String? errorMessage;
+
+  Future<void> getRecommendations(String severity) async {
+    await Future.delayed(const Duration(seconds: 2)); // Simulate a network delay
+    setState(() {
+      isLoading = true;
+      errorMessage = null;
+      recommendation = null;
+    });
 
     try {
-      
       const apiKey = 'AIzaSyBMtpXp1vj-mH_p0649-qU49NHqwnAl6QQ';
 
       if (apiKey == null) {
         throw Exception('API_KEY environment variable not found');
       }
 
-      final model =
-          GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+      final model = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
       final content = [
         Content.text(
             'Provide remedies for Black Sigatoka disease based on the $severity severity level')
       ];
       final response = await model.generateContent(content);
-      return response.text;
+      setState(() {
+        recommendation = response.text;
+      });
     } catch (error) {
       log(error.toString()); // Log the error for debugging
-      return "An error occurred while fetching recommendations.";
+      setState(() {
+        errorMessage = "An error occurred while fetching recommendations.";
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -45,93 +59,68 @@ class _RecommendationScreenState extends State<RecommendationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      backgroundColor: Colors.white,
-      title: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 45, vertical: 30),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Image.asset(
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
                 'assets/images/Logo.png',
                 height: 30,
                 width: 30,
               ),
-            ),
-            const SizedBox(width: 5),
-            Center(
-              child: Text(
-                'Recommend'.toUpperCase(),
+              const SizedBox(width: 5),
+              Text(
+                'Recommendations'.toUpperCase(),
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                     color: Colors.black),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              'Black Sigatoka Severity: ${widget.diseaseSeverity}',
-              style: const TextStyle(fontSize: 18.0),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () async {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return const AlertDialog(
-                      title: Text('Fetching Recommendations...'),
-                      content: Row(
-                        children: [
-                          CircularProgressIndicator(),
-                          SizedBox(width: 10.0),
-                          Text('Please wait'),
-                        ],
-                      ),
-                    );
-                  },
-                );
-
-                String? recommendation =
-                    await getRecommendations(widget.diseaseSeverity);
-                Navigator.pop(context); // Close the initial progress dialog
-
-                if (recommendation != null) {
-                  List<String> recommendations = recommendation.split('.');
-                  String firstRecommendation = recommendations.first.trim();
-
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('AI Chatbot Recommendations'),
-                        content: Text(firstRecommendation),
-                      );
-                    },
-                  );
-                } else {
-                  showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Error'),
-                        content: const Text('Failed to fetch recommendations.'),
-                      );
-                    },
-                  );
-                }
-              },
-              child: const Text('Get Recommendations'),
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Black Sigatoka Severity: ${widget.diseaseSeverity}',
+                style: const TextStyle(fontSize: 15.0),
+              ),
+              const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () async {
+                  await getRecommendations(widget.diseaseSeverity);
+                },
+                child: const Text('Get Recommendations'),
+              ),
+              const SizedBox(height: 20.0),
+              if (isLoading)
+                Row(
+                  children: const [
+                    CircularProgressIndicator(),
+                    SizedBox(width: 10.0),
+                    Text('Fetching Recommendations...'),
+                  ],
+                ),
+              if (recommendation != null)
+                Text(
+                  'Recommendation: $recommendation',
+                  style: const TextStyle(fontSize: 16.0),
+                ),
+              if (errorMessage != null)
+                Text(
+                  'Error: $errorMessage',
+                  style: const TextStyle(fontSize: 16.0, color: Colors.red),
+                ),
+            ],
+          ),
         ),
       ),
     );
