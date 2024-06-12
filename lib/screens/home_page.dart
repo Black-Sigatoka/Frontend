369688e3eb26a9e7ba0c0e27eff92de939a5d119
Peter Kyeyune <peterkayk@gmail.com>
2024-06-12 12:57:19 +0300

    "Refactored home page layout: replaced Column with SingleChildScrollView, updated logo text, and split title into two lines."

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -21,54 +21,65 @@ class _HomeScreenState extends State<HomeScreen> {
         } else {
           return Scaffold(
             body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset(
-                    'assets/images/Logo.png',
-                    height: 150,
-                  ),
-                  const SizedBox(height: 20),
-                  Text(
-                    'Sigatoka Mobile Application'.toUpperCase(),
-                    style: const TextStyle(
-                        fontSize: 18,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(height: 25),
-                  const Text(
-                    'This is a mobile application dedicated to diagnosing and managing Black Leaf Streak efficiently. It harnesses advanced image recognition to identify disease symptoms. The applications provide detailed information about the severity of the disease and recommended actions.',
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        backgroundColor:
-                            const Color.fromARGB(255, 127, 181, 230)),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
-                          ));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 95, vertical: 15),
-                      child: const Text(
-                        'Get Started',
-                        style: TextStyle(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/Logo.png',
+                      height: 150,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      'Black Leaf Streak Diagnosis'
+                          .toUpperCase(),
+                      style: const TextStyle(
                           fontSize: 18,
-                          fontWeight: FontWeight.w500,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'Mobile Application'
+                          .toUpperCase(),
+                      style: const TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w600),
+                    ),
+                    const SizedBox(height: 25),
+                    const Text(
+                      'This is a mobile application dedicated to diagnosing and managing Black Leaf Streak efficiently. It harnesses advanced image recognition to identify disease symptoms. The application provides detailed information about the severity of the disease and recommended actions.',
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 30),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          backgroundColor:
+                              const Color.fromARGB(255, 127, 181, 230)),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 95, vertical: 15),
+                        child: const Text(
+                          'Get Started',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           );
