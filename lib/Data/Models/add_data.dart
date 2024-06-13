2e2c492a6763a89817f48b1611790442f6405182
Peter Kyeyune <peterkayk@gmail.com>
2024-06-13 09:04:28 +0300

    "Updated API key in StoreData class"

--- lib/Data/Models/add_data.dart
+++ lib/Data/Models/add_data.dart
@@ -42,7 +42,7 @@ class StoreData {
   Future<dynamic> sendInferenceRequest(String imageUrl) async {
     const endpointUrl =
         "https://yolo-endpoint.westus.inference.ml.azure.com/score";
-    const apiKey = "C8cBSzqwQSo7VMejvtcv2lFTfxICFzXW"; // Replace with your API key
+    const apiKey = "MZoadJCWfyBmKHfGnMgcHwcvlY3ZrmHf"; // Replace with your API key
 
     final headers = {
       'Content-Type': 'application/json',
